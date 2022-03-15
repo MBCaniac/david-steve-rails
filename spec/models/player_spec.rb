87d6eb0cd257ff6083f8422103597fd1a897e8a5
Steven Mickelson <caniac@Stevens-MacBook-Air.local>
2022-03-15 09:42:42 -0500

    updated database

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: players
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  number     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do

f6255b69789704a121d571bc0e09a75c3f1fd391
Steven Mickelson <caniac@Stevens-MacBook-Air.local>
2022-03-17 10:27:23 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :players
-<<<<<<< HEAD
 
   get '/s/:slug', to: 'links#show', as: :short
-=======
->>>>>>> b927c0341583e7d9a3ebfab98ab6bf5d44d6adf4
 end
